c06d547f8eeaa7fe4b8f93f122c15bbf332009fe
Edgar <edgarpinheiro3@gmail.com>
2023-05-29 14:25:45 -0300

    AULA: 02 - MIDDLEWARE TENANT NO LARAVEL

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/404-tenant', function () {
+    view('errors.404-tenant');
+})->name('404.tenant');
+
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
