e609dbcd3b0fdbf7c69d3c44f46cb1ec8c70e74c
Edgar <edgarpinheiro3@gmail.com>
2023-05-30 15:36:36 -0300

    AULA: 08 - TENANT ROTAS E MIDDLEWARE TENANT MAIN NO LARAVEL

---
+++ app/Http/Middleware/Tenant/CheckDomainMain.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware\Tenant;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class CheckDomainMain
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+
+        if( request()->getHost() != config('tenant.domain_main') ) {
+
+            abort(410);
+
+        }
+
+        return $next($request);
+    }
+}
--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -46,6 +46,12 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('web')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
+            
+            Route::prefix('tenants')
+                ->middleware('web')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/tenant.php'));
+
         });
     }
 
